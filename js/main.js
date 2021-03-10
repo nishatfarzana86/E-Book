6b4b82694c214d07504d45735f15d501a373d12b
nishat <nishatfarzana310@gmail.com>
2021-03-10 20:15:29 +0600

    E-Book

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*Sticky navigation*/
+let navbar = $(".navbar");
+
+$(window).scroll(function(){
+	let oTop = $(".section-2").offset().top-window.innerHeight;
+	if($(window).scrollTop()>oTop){
+		navbar.addClass("sticky");
+	}else{
+		navbar.removeClass("sticky");
+	}
+});
+
+/*Counter Animation*/
+
+let nCount = function(selector){
+	$(selector).each(function(){
+		$(this).animate({
+			Counter:$(this).text()
+		}.{
+			duration:4000,
+			easing:"swing",
+			step:function(value){
+				$(this).text(Math.ceil(value));
+			}
+		}
+		})
+	});
+}
+
+$(window).scroll(function(){
+	let oTop=$(".numbers").offset().top-windo.innerHeight;
+	if(a==0 && $(window).scrollTop()>=oTop){
+		a++;
+		nCount(".rect > h1");
+	}
+})
